e9c2599bcd8b191041e03f38cf3a4f8cfd7948c9
Cesc Blanco <cesc.blanco98@gmail.com>
2025-06-10 12:50:35 +0200

    agregacion funciones en __init__

--- fbref_scraper/__init__.py
+++ fbref_scraper/__init__.py
@@ -1,2 +1,3 @@
 from .league_manager import LeagueManager
-from .player_data import get_players_data, limpieza_df_players, creacion_df_jugadores_estadistica_unica
+from .player_data import get_players_data, limpieza_df_players, creacion_df_jugadores_estadistica_unica, obtener_jugadores_similares, obtener_tabla_datos_jugador_por90_percentiles
+from .team_data import obtener_tabla_liga_principal, obtener_tabla_equipos_estadistica_unica, obtener_tabla_tiros_partido, limpiar_df_estadisticas_partido,bajada_nivel_porteros,  obtener_tabla_estadisticas_principales_partido
