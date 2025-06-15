90698ef8885c9746010c1f232e6443f038eeeee0
Cesc Blanco <cesc.blanco98@gmail.com>
2025-06-15 12:59:45 +0200

    cambio init

--- fbref_scraper/__init__.py
+++ fbref_scraper/__init__.py
@@ -10,8 +10,6 @@ listos para ser utilizados en análisis posteriores.
 from .league_manager import LeagueManager
 
 from .player_data import (
-    get_players_data,
-    limpieza_df_players,
     creacion_df_jugadores_estadistica_unica,
     creacion_df_players_torneo_fbref,
     creacion_df_porteros_torneo_fbref,
@@ -23,26 +21,21 @@ from .team_data import (
     obtener_tabla_liga_principal,
     obtener_tabla_equipos_estadistica_unica,
     obtener_tabla_tiros_partido,
-    limpiar_df_estadisticas_partido,
-    bajada_nivel_porteros,
     obtener_tabla_estadisticas_principales_partido,
 
     )
 
 __all__ = [
     "LeagueManager",
-    "get_players_data",
-    "limpieza_df_players",
     "creacion_df_jugadores_estadistica_unica",
     "creacion_df_players_torneo_fbref",
     "creacion_df_porteros_torneo_fbref",
     "obtener_jugadores_similares",
     "obtener_tabla_datos_jugador_por90_percentiles",
+
     "obtener_tabla_liga_principal",
     "obtener_tabla_equipos_estadistica_unica",
     "obtener_tabla_tiros_partido",
-    "limpiar_df_estadisticas_partido",
-    "bajada_nivel_porteros",
     "obtener_tabla_estadisticas_principales_partido",
 
     ]
